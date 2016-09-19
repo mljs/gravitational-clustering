2b9b0738b1c3fda31e4b9468320ac17df0c21e9b
jeffersonh44 <jeffersonh44@gmail.com>
2016-09-19 17:18:06 -0500

    feat: basic implementation complete

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var GravitationalClustering = require('..');
 
 describe('basic implementation', function () {
 
-    it('main', function () {
+    it('Linear separable clusters', function () {
         var clusters = 3;
         var size = 5;
         var arr = new Array(clusters * size);
@@ -25,4 +25,32 @@ describe('basic implementation', function () {
             }
         }
     });
+    
+    it('Non-linear clusters', function () {
+        var size = 200;
+        var arr = new Array(size * 5);
+        for(var i = 0; i < size; ++i) {
+            arr[i] = [random(0, 20),random(0, 1)];
+            arr[200 + i] = [random(0, 1),random(0, 20)];
+            arr[400 + i] = [random(19, 20),random(0, 20)];
+            arr[600 + i] = [random(0, 20),random(19, 20)];
+            arr[800 + i] = [random(9, 11), random(9, 11)];
+        }
+
+        var gc = new GravitationalClustering({}, arr);
+        var result = gc.run(45);
+
+        for(i = 0; i < 800; i++) {
+            result.y[i].should.be.equal(0);
+        }
+        for(i = 800; i < 1000; i++) {
+            result.y[i].should.be.equal(1);
+        }
+    })
 });
+
+
+function random(min, max)
+{
+    return Math.random()*(max-min)+min;
+}
