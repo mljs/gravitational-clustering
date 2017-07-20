f5c18f66d4f72ffe31807fabd11fc28dfa5de93b
Jefferson Hernández <jeffersonh44@gmail.com>
2017-07-20 16:53:53 -0500

    test: fix error on non linear test

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -38,13 +38,38 @@ describe('basic implementation', function () {
         var gc = new GravitationalClustering();
         var result = gc.train(arr);
 
+        var prediction = result.y[0];
+        for (i = 1; i < 800; i++) {
+            expect(result.y[i]).toBe(prediction);
+        }
+
+        prediction = result.y[800];
+        for (i = 801; i < 1000; i++) {
+            expect(result.y[i]).toBe(prediction);
+        }
+    });
+
+    test('Load and save', function () {
+        var size = 200;
+        var arr = new Array(size * 5);
+        for (var i = 0; i < size; ++i) {
+            arr[i] = [random(0, 20), random(0, 1)];
+            arr[200 + i] = [random(0, 1), random(0, 20)];
+            arr[400 + i] = [random(19, 20), random(0, 20)];
+            arr[600 + i] = [random(0, 20), random(19, 20)];
+            arr[800 + i] = [random(9, 11), random(9, 11)];
+        }
+
+        var gc = new GravitationalClustering();
+        var result = gc.train(arr);
+
         for (i = 0; i < 800; i++) {
             expect(result.y[i]).toBe(0);
         }
         for (i = 800; i < 1000; i++) {
             expect(result.y[i]).toBe(1);
         }
-    });
+    })
 });
 
 
